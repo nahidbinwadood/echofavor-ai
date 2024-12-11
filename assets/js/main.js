7888c629c42e5547085ec9310c918df5ed0139d0
User <nahidbinwadood@gmail.com>
2024-12-11 17:40:58 +0600

    updaded features section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,22 @@
 $(document).ready(function () {
   // play the video:
-  // Get the elements
-  const playBtn = document.getElementById('play-btn');
-  const pauseBtn = document.getElementById('pause-btn');
-  const video = document.getElementById('hero-video');
+   // Get the elements
+const playBtn = document.getElementById('play-btn');
+const pauseBtn = document.getElementById('pause-btn');
+const heroVideo = document.getElementById('hero-video');
 
-  // Function to play the video and toggle buttons
-  playBtn.addEventListener('click', () => {
-    // Show the video and play it
-    video.classList.remove('hidden');
-    video.play();
+// Function to play the video and toggle buttons
+playBtn?.addEventListener('click', () => {
+  heroVideo.loop = true;
+  heroVideo.play();
+  playBtn.classList.add('hidden');
+  pauseBtn.classList.remove('hidden');
+});
 
-    // Hide the play button and show the pause button
-    playBtn.classList.add('hidden');
-    pauseBtn.classList.remove('hidden');
-  });
-
-  // Function to pause the video and toggle buttons
-  pauseBtn.addEventListener('click', () => {
-    // Pause the video
-    video.pause();
-
-    // Show the play button and hide the pause button
-    playBtn.classList.remove('hidden');
-    pauseBtn.classList.add('hidden');
-  });
+// Function to pause the video and toggle buttons
+pauseBtn?.addEventListener('click', () => {
+  heroVideo.pause();
+  playBtn.classList.remove('hidden');
+  pauseBtn.classList.add('hidden');
+});
 });
