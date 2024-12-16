16381cd022cf4c73085da2211f3ae3b6f75f376b
User <nahidbinwadood@gmail.com>
2024-12-16 17:37:29 +0600

    added dashboard homepage

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -9,9 +9,29 @@ $(document).ready(function () {
     profileMenuContainer.classList.toggle('active');
   });
 
-  document.addEventListener('click', (e)=>{
-    if(!profileButton.contains(e.target) && !profileMenuContainer.contains(e.target)){
-        profileMenuContainer.classList.remove('active');
+  document.addEventListener('click', (e) => {
+    if (
+      !profileButton.contains(e.target) &&
+      !profileMenuContainer.contains(e.target)
+    ) {
+      profileMenuContainer.classList.remove('active');
     }
-  })
+  });
+
+  //chat history::options:
+
+  const chatHistoryOptionBtn = document.querySelector(
+    '.dashboard-chat-history-item-options'
+  );
+  const chatHistoryOption = document.querySelector(
+    '.dashboard-chat-item-option-wrapper'
+  );
+  chatHistoryOptionBtn?.addEventListener('click', () => {
+    chatHistoryOption.classList.toggle('active');
+  });
+  document.addEventListener('click', (e) => {
+    if (!chatHistoryOptionBtn.contains(e.target)) {
+      chatHistoryOption.classList.remove('active');
+    }
+  });
 });
